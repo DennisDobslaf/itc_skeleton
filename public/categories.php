139d8f8e60dfcadcf99ed5ba2fdc7ca51faeab87
Dennis Dobslaf <dennis.dobslaf@gmail.com>
2021-11-13 09:27:03 +0100

    Aufsplittung in einzelne Templates

---
+++ public/categories.php
@@ -0,0 +1,14 @@
+<?php
+include __DIR__ . '/../templates/html_header.phtml';
+include __DIR__ . '/../templates/page_navigation.phtml';
+include __DIR__ . '/../templates/page_header.phtml';
+?>
+<!-- Main Content-->
+<div class="container px-4 px-lg-5">
+    Categories...
+</div>
+
+<?php
+include __DIR__ . '/../templates/page_footer.phtml';
+include __DIR__ . '/../templates/html_footer.phtml';
+?>
